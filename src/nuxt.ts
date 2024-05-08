f739c6f4dd7d35eeefecc8a75b5c1f894e44fbc5
MorevM <max.seainside@gmail.com>
2024-05-08 12:34:43 +0300

    fix(nuxt): Ensure Nuxt version is running normally

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -1,5 +1,10 @@
 import { addPluginTemplate, createResolver, defineNuxtModule, isNuxt2 } from '@nuxt/kit';
+import { isArray } from '@morev/utils';
 import type { PartialOptions } from './types';
+
+const BABEL_PLUGIN_NAME = '@babel/plugin-transform-logical-assignment-operators';
+const SCOPE = '@morev';
+const MODULE_NAME = `${SCOPE}/smooth-scroll`;
 
 export default defineNuxtModule<PartialOptions>({
 	meta: {
@@ -13,6 +18,24 @@ export default defineNuxtModule<PartialOptions>({
 	hooks: {},
 	setup(options, nuxt) {
 		const resolver = createResolver(import.meta.url);
+
+		// This is necessary because the package uses utilities
+		// that use modern syntax and have not been transpiled.
+		if (isNuxt2()) {
+			nuxt.options.build.transpile.push('@morev/utils', 'ohash', MODULE_NAME);
+
+			/* @ts-expect-error -- Lack of compatibility with Nuxt 2 */
+			nuxt.options.build.babel.plugins ??= [];
+			/* @ts-expect-error -- Lack of compatibility with Nuxt 2 */
+			const doesBabelPluginExists = nuxt.options.build.babel.plugins.some((plugin) => {
+				return isArray(plugin)
+					? plugin[0] === BABEL_PLUGIN_NAME
+					: plugin === BABEL_PLUGIN_NAME;
+			});
+
+			/* @ts-expect-error -- Lack of compatibility with Nuxt 2 */
+			!doesBabelPluginExists && nuxt.options.build.babel.plugins.push(BABEL_PLUGIN_NAME);
+		}
 
 		addPluginTemplate({
 			src: resolver.resolve('nuxt-plugin.js'),
